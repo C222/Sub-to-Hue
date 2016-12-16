59bd05c908bcb464b91632534c0bc1fb94d2e319
C222 <c222streams@gmail.com>
2016-12-16 13:27:15 -0800

    Adding color reference page

--- scripts/color/color.js
+++ scripts/color/color.js
@@ -3,7 +3,7 @@
  * Version 0.2.1.2
  *
  * 2009-09-12
- * 
+ *
  * By Eli Grey, http://eligrey.com
  * Licensed under the MIT License
  *   See https://github.com/eligrey/color.js/blob/master/LICENSE.md
@@ -25,7 +25,7 @@ var Color = (function () {
 		parseHex = function (h) {
 			return parseInt(h, 16);
 		};
-		
+
 		if (args < 3) { // called as Color(color [, alpha])
 			if (typeof r === str) {
 				r = r.substr(r.indexOf("#") + 1);
@@ -34,19 +34,19 @@ var Color = (function () {
 				threeDigits &&
 					(r = (((r & 0xF00) * 0x1100) | ((r & 0xF0) * 0x110) | ((r & 0xF) * 0x11)));
 			}
-			
+
 			args === 2 && // alpha specifed
 				(a = g);
-			
+
 			g = (r & 0xFF00) / 0x100;
 			b =  r & 0xFF;
 			r =  r >>> 0x10;
 		}
-		
+
 		if (!(color instanceof Color)) {
 			return new Color(r, g, b, a);
 		}
-		
+
 		this.channels = [
 			typeof r === str && parseHex(r) || r,
 			typeof g === str && parseHex(g) || g,
@@ -60,20 +60,20 @@ var Color = (function () {
 	lowerCase   = "toLowerCase",
 	math        = Math,
 	colorDict;
-	
+
 	// RGB to HSL and HSL to RGB code from
 	// http://www.mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-	
+
 	Color.RGBtoHSL = function (rgb) {
 		// in JS 1.7 use: var [r, g, b] = rgb;
 		var r = rgb[0],
 		    g = rgb[1],
 		    b = rgb[2];
-		
+
 		r /= 255;
 		g /= 255;
 		b /= 255;
-		
+
 		var max = math.max(r, g, b),
 		    min = math.min(r, g, b),
 		h, s, l = (max + min) / 2;
@@ -94,15 +94,15 @@ var Color = (function () {
 		return [h, s, l];
 
 	};
-	
+
 	Color.HSLtoRGB = function (hsl) {
 		// in JS 1.7 use: var [h, s, l] = hsl;
 		var h = hsl[0],
 		    s = hsl[1],
 		    l = hsl[2],
-		
+
 		r, g, b,
-		
+
 		hue2rgb = function (p, q, t){
 		    if (t < 0) {
 		    	t += 1;
@@ -121,7 +121,7 @@ var Color = (function () {
 		    }
 		    return p;
 		};
-		
+
 		if (s === 0) {
 			r = g = b = l; // achromatic
 		} else {
@@ -135,36 +135,36 @@ var Color = (function () {
 
 		return [r * 0xFF, g * 0xFF, b * 0xFF];
 	};
-	
+
 	Color.rgb = function (r, g, b, a) {
 		return new Color(r, g, b, typeof a !== undef ? a : 1);
 	};
-	
+
 	Color.hsl = function (h, s, l, a) {
 		var rgb = Color.HSLtoRGB([h, s, l]),
 		   ceil = math.ceil;
 		return new Color(ceil(rgb[0]), ceil(rgb[1]), ceil(rgb[2]), typeof a !== undef ? a : 1);
 	};
-	
+
 	Color.TO_STRING_METHOD = "hexTriplet"; // default toString method used
-	
+
 	Color.parse = function (color) {
 		color = color.replace(/^\s+/g, "") // trim leading whitespace
 			[lowerCase]();
-		
+
 		if (color[0] === "#") {
 			return new Color(color);
 		}
-		
+
 		var cssFn = color.substr(0, 3), i;
-		
+
 		color = color.replace(/[^\d,.]/g, "").split(",");
 		i     = color.length;
-		
+
 		while (i--) {
 			color[i] = color[i] && parseFloat(color[i]) || 0;
 		}
-		
+
 		switch (cssFn) {
 			case "rgb": // handle rgb[a](red, green, blue [, alpha])
 				return Color.rgb.apply(Color, color); // no need to break;
@@ -174,34 +174,38 @@ var Color = (function () {
 				color[2] /= 100;
 				return Color.hsl.apply(Color, color);
 		}
-		
+
 		return null;
 	};
-	
+
 	(Color.clearColors = function () {
 		colorDict = {
 			transparent: [0, 0, 0, 0]
 		};
 	})();
-	
+
 	Color.define = function (color, rgb) {
 		colorDict[color[lowerCase]()] = rgb;
 	};
-	
+
 	Color.get = function (color) {
 		color = color[lowerCase]();
-		
+
 		if (Object.prototype.hasOwnProperty.call(colorDict, color)) {
 			return Color.apply(null, [].concat(colorDict[color]));
 		}
-		
+
 		return null;
 	};
-	
+
+	Color.all = function () {
+		return colorDict;
+	};
+
 	Color.del = function (color) {
 		return delete colorDict[color[lowerCase]()];
 	};
-	
+
 	Color.random = function (rangeStart, rangeEnd) {
 		typeof rangeStart === str &&
 			(rangeStart = Color.get(rangeStart)) &&
@@ -209,10 +213,10 @@ var Color = (function () {
 		typeof rangeEnd === str &&
 			(rangeEnd = Color.get(rangeEnd)) &&
 			(rangeEnd = rangeEnd.getValue());
-		
+
 		var floor = math.floor,
 		   random = math.random;
-		
+
 		rangeEnd = (rangeEnd || 0xFFFFFF) + 1;
 		if (!isNaN(rangeStart)) {
 			return new Color(floor((random() * (rangeEnd - rangeStart)) + rangeStart));
@@ -220,11 +224,11 @@ var Color = (function () {
 		// random color from #000000 to #FFFFFF
 		return new Color(floor(random() * rangeEnd));
 	};
-	
+
 	proto.toString = function () {
 		return this[Color.TO_STRING_METHOD]();
 	};
-	
+
 	proto.valueOf = proto.getValue = function () {
 		var channels = this.channels;
 		return (
@@ -233,17 +237,17 @@ var Color = (function () {
 			 channels[2]
 		);
 	};
-	
+
 	proto.setValue = function (value) {
 		this.channels.splice(
 			0, 3,
-			
+
 			value >>> 0x10,
 			(value & 0xFF00) / 0x100,
 			value & 0xFF
 		);
 	};
-	
+
 	proto.hexTriplet = ("01".substr(-1) === "1" ?
 	// pad 6 zeros to the left
 		function () {
@@ -255,39 +259,39 @@ var Color = (function () {
 			return "#" + (new Array( str.length < 6 ? 6 - str.length + 1 : 0)).join("0") + str;
 		}
 	);
-	
+
 	proto.css = function () {
 		var color = this;
 		return color.channels[3] === 1 ? color.hexTriplet() : color.rgba();
 	};
-	
+
 	// TODO: make the following functions less redundant
-	
+
 	proto.rgbData = function () {
 		return this.channels.slice(0, 3);
 	};
-	
+
 	proto.hslData = function () {
 		return Color.RGBtoHSL(this.rgbData());
 	};
-	
+
 	proto.rgb = function () {
 		return "rgb(" + this.rgbData().join(",") + ")";
 	};
-	
+
 	proto.rgba = function () {
 		return "rgba(" + this.channels.join(",") + ")";
 	};
-	
+
 	proto.hsl = function () {
 		var hsl = this.hslData();
 		return "hsl(" + hsl[0] * 360 + "," + (hsl[1] * 100) + "%," + (hsl[2] * 100) + "%)";
 	};
-	
+
 	proto.hsla = function () {
 		var hsl = this.hslData();
 		return "hsla(" + hsl[0] * 360 + "," + (hsl[1] * 100) + "%," + (hsl[2] * 100) + "%," + this.channels[3] + ")";
 	};
-	
+
 	return Color;
 }());
